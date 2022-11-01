415a091f4c7dde6d054c472d0d486ea377a9bfc0
canozyigiit <muhammetcanozyigit@gmail.com>
2022-11-01 23:58:57 +0300

    PrimeNumber added

---
+++ src/PrimeNumber/PrimeNumber.java
@@ -0,0 +1,22 @@
+package PrimeNumber;
+
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+        int count = 0;
+
+        for (int i = 2 ; i <= 100 ; i++) {
+            count = 0;
+
+            for (int j = 2 ; j <= i ; j++) {
+                if (i % j == 0 ) {
+                    count++;
+                }
+            }
+
+            if (count == 1) {
+                System.out.println(i);
+            }
+        }
+    }
+}
