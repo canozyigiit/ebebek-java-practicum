10f123fb0380249d52fc0f5fc867f3a5e27b8d15
canozyigiit <muhammetcanozyigit@gmail.com>
2022-11-04 13:07:28 +0300

    ArrayMatrix added

---
+++ src/ArrayMatrix/ArrayMatrix.java
@@ -0,0 +1,37 @@
+package ArrayMatrix;
+
+public class ArrayMatrix {
+
+    public static int[][] transposeMatrix(int[][] matrix) {
+
+        int[][] transpose = new int[matrix[0].length][matrix.length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+
+        return transpose;
+    }
+
+    public static void printMatrix(int[][] matrix) {
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[][] matrix = { { 2, 3, 4 }, { 5, 6, 4 } };
+        System.out.println("Matris");
+        printMatrix(matrix);
+        System.out.println("Transpozunu");
+        printMatrix(transposeMatrix(matrix));
+
+    }
+}
